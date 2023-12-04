11692ef11c73554266400d2a318a484bb4db8097
Ygor Combi <ycombi349@gmail.com>
2023-12-04 08:12:54 -0300

    FEAT: header admin and page login melhorias

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,7 @@
-<x-layout title="Admin">
+<x-layout title="Admin | Ygor Combi">
   <main class="index bg-pattern" style="--icon-url:url('{{ Vite::image('icons/pattern-dot.svg') }}')">
-    vc está logado
+    <section class="container">
+      vc está logado
+    </section>
   </main>
 </x-layout>
