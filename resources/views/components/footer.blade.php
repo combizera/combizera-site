a4e614c9f10a390cafa65f853e0ab0db95217a14
Ygor Combi <ycombi349@gmail.com>
2024-01-14 10:24:12 -0300

    FEAT: links and footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -8,17 +8,17 @@
           <nav>
             <ul class="ul">
               <li>
-                <a href="#">- Projetos</a>
+                <a href="#">Projetos</a>
               </li>
               <li>
-                <a href="#">- Sobre</a>
+                <a href="#">Sobre</a>
               </li>
               <li>
-                <a href="#">- Contato</a>
+                <a href="#">Contato</a>
               </li>
               @guest
                 <li>
-                  <a href="/login">- Login</a>
+                  <a href="/login">Login</a>
                 </li>
               @endguest
             </ul>
@@ -29,7 +29,7 @@
         <nav class="footer__social">
           <ul class="ul grid-3">
             <li>
-              <a href="https://instagram/combizera">
+              <a target="_blank" href="https://instagram/combizera">
                 <x-icons.insta></x-icons.insta>
                 Instagram
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -38,7 +38,7 @@
               </a>
             </li>
             <li>
-              <a href="https://instagram/combizera">
+              <a target="_blank" href="https://www.linkedin.com/in/combizera/">
                 <x-icons.linkedin></x-icons.linkedin>
                 Linkedin
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -47,7 +47,7 @@
               </a>
             </li>
             <li>
-              <a href="http://instagram/combizera">
+              <a href="#">
                 <x-icons.pdf></x-icons.pdf>
                 CV.pdf
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -56,7 +56,7 @@
               </a>
             </li>
             <li>
-              <a href="http://instagram/combizera">
+              <a target="_blank" href="http://be.net/combizera">
                 <x-icons.behance></x-icons.behance>
                 Behance
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -65,7 +65,7 @@
               </a>
             </li>
             <li>
-              <a href="http://instagram/combizera">
+              <a target="_blank" href="http://wa.me/5553999902079">
                 <x-icons.zap></x-icons.zap>
                 Whatsapp
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -74,7 +74,7 @@
               </a>
             </li>
             <li>
-              <a href="http://instagram/combizera">
+              <a target="_blank" href="mailto:ola@combizera.com">
                 <x-icons.email></x-icons.email>
                 Email
                 <svg id="arrow" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -102,8 +102,6 @@
     <div class="footer__copy border">
       <p>
         Ygor Combi // {{ date('Y') }} // Todos os Direitos Reservados</p>
-      {{-- DESENVOLVIDO POR --}}
-      {{-- <a href="https://combizera.com" target="_blank">Desenvolvido por Combizera</a> --}}
     </div>
   </div>
 </footer>
