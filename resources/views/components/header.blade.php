3c39fd8a2585a8022e15a4dd8578e27ee49cd58b
Ygor Combi <ycombi349@gmail.com>
2024-02-03 21:49:16 -0300

    FEAT: melhorias header admin

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,20 +1,45 @@
 <header id="header" class="border-bottom">
   @auth
-  <div class="header__admin">
-    <nav class="header__nav container">
+  <div class="header__admin space-between">
+    <nav>
       <ul class="ul">
-        <li>
-          <a href="{{ route('admin') }}">Editar Seções</a>
-        </li>
         <li>
           <a
             href="{{ route('logout')}}"
             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <span class="material-symbols-outlined">
+              logout
+            </span>
             Deslogar
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('admin') }}">
+            <span class="material-symbols-outlined">
+              edit
+            </span>
+            Editar Seções
+          </a>
+        </li>
+        <li>
+          <a href="{{ route('admin') }}">
+            <span class="material-symbols-outlined">
+              note_add
+            </span>
+            Cadastrar Projeto
           </a>
         </li>
       </ul>
     </nav>
+    <div class="header__profile">
+      <a href="">
+        <p>Olá,
+          <strong>
+            Ygor Combi
+          </strong>
+        </p>
+      </a>
+    </div>
   </div>
 
   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
