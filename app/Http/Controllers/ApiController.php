6a7c566463e012c759f18cc4678845caed129a20
combizera <ycombi349@gmail.com>
2024-03-27 00:24:05 -0300

    FEAT: api chamando apenas maestria dos champs

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,15 +12,31 @@ class ApiController extends Controller
     return view('lol');
   }
 
+  // public function maestria(Request $request)
+  // {
+  //   $payload = $request->only([
+  //     "name",
+  //     "tag",
+  //   ]);
+
+  //   $resultSummoner = Http::withHeader("X-Riot-Token", env("API_RIOT"))->get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{$payload['name']}/{$payload['tag']}");
+
+  //   dd($resultSummoner->json());
+  // }
+
   public function maestria(Request $request)
   {
-    $payload = $request->only([
-      "name",
-      "tag",
-    ]);
+    $encrypetedSummonerId = "T1p1AMGkELObSrHIn0qpYAzpxEU2NFtlfIx_l00O8HEH3tUiHpGJYhQyu5pIncB5go9e9MayYwTgjA";
 
-    $resultSummoner = Http::withHeader("X-Riot-Token", env("API_RIOT"))->get("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{$payload['name']}/{$payload['tag']}");
+    // dd($encrypetedSummonerId);
 
-    dd($resultSummoner->json());
+    $resultSummoner = Http::withHeader("X-Riot-Token", env("API_RIOT"))->get("https://br1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{$encrypetedSummonerId}");
+
+    if ($resultSummoner->successful()) {
+      $maestriaData = $resultSummoner->json();
+      dd($maestriaData);
+    } else {
+      return response()->json(['error' => 'Falha ao obter dados de maestria'], $resultSummoner->status());
+    }
   }
 }
